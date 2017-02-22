144449305c68520bb20a6431e3b494a94d3fde68
CodigoG <CodigoG@Progra>
2017-02-22 00:33:02 -0600

    fin

---
+++ src/practica1s12017_201403877/Practica1s12017_201403877.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1s12017_201403877;
+
+/**
+ *
+ * @author CodigoG
+ */
+public class Practica1s12017_201403877 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MenuPrincipal nuevo = new MenuPrincipal();
+        nuevo.setVisible(true);
+    }
+}
